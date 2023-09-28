e3ea83e4a403dad8b9684647ed3ce283589ec78f
Rui Borges <ruiborges206@gmail.com>
2023-09-28 12:07:59 +0100

    -Created Game glass and implemented draw() and run() methods.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,4 @@
 package org.example;
-
 import com.googlecode.lanterna.TextCharacter;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
@@ -11,23 +10,8 @@ import java.io.IOException;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
-        Screen screen = null;
-        try {
-            Terminal terminal = new DefaultTerminalFactory().createTerminal();
-            screen = new TerminalScreen(terminal);
-            screen.setCursorPosition(null); // we don't need a cursor
-            screen.startScreen(); // screens must be started
-            screen.doResizeIfNecessary(); // resize screen if necessary
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        screen.clear();
-        screen.setCharacter(40, 10, TextCharacter.fromCharacter('X')[0]);
-        try {
-            screen.refresh();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+    public static void main(String[] args) throws IOException {
+        Game game = new Game();
+        game.run();
     }
 }
